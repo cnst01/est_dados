b36c56dc1eb20ac75a8a4bd3010b302439ff8a63
cnst01 <cassionst@gmail.com>
2023-06-11 22:51:46 -0300

    adicionei a questao 5 mais perto da correta

--- revisao/exerc5/ex5.c
+++ revisao/exerc5/ex5.c
@@ -3,29 +3,28 @@
 typedef struct // Cria uma STRUCT para armazenar os dados de uma pessoa
 {
     char * nome[100];
-    float Peso;   // define o campo Peso
     int Idade;    // define o campo Idade
-    float Altura; // define o campo Altura
 } Pessoa; // Define o nome do novo tipo criado
 
 void ImprimePessoa(Pessoa P)
 {
-  printf("Nome: %s, Idade: %d  Peso: %f Altura: %f\n", *P.nome, P.Idade, P.Peso, P.Altura);
+  printf("Nome: %s, Idade: %d\n", *P.nome, P.Idade);
 }
 
-void SetPessoa(Pessoa *P, int idade, float peso, float altura)
+void SetPessoa(Pessoa *P, char nome[], int idade)
 { // Nesta função o parâmetro P é um ponteiro para uma struct
-    *P->nome = "Joao";
-    (*P).Idade = idade;  // o campo pode ser acessado desta forma
-    P->Peso = peso;      // ou desta
-    P->Altura = altura;
+    *P->nome = nome;
+    (*P).Idade = idade;
 }
    
 int main()
 {
     Pessoa Joao;
-    SetPessoa(&Joao, 15, 70.5, 1.75);
+    Pessoa Cassio;
+    SetPessoa(&Joao, "joao", 15);
+    SetPessoa(&Cassio, "Cassio", 21);
     ImprimePessoa(Joao);
+    ImprimePessoa(Cassio);
 
     return 0;
 }
